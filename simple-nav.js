d998a38a5669705561ae29c9442c84d0c59dc8c2
gijsroge <rogegijs@gmail.com>
2016-09-18 12:48:15 +0200

    wip

--- simple-nav.js
+++ simple-nav.js
@@ -1,16 +1,19 @@
 /**
  * SimpleNav
  */
-(function ( $ ) {
+(function ($) {
+
 
     $.fn.simplenav = function (options) {
 
-        var settings = $.extend({
-            selector: "#556b2f"
-        }, options );
-
         var simplenavElement = $(this);
         var support = typeof(Storage) !== "undefined";
+        var settings = $.extend({
+            toggle: 'js-simplenav-toggle',
+            toggleWrapper: 'js-simplenav-wrapper',
+            dropdown: 'js-simplenav-dropdown',
+            throttle: 250
+        }, options);
 
         if (!support) {
             console.warn('sorry this browser sucks');
@@ -20,8 +23,11 @@
         var app = {
             init: function () {
                 var _this = this;
+                var instance = 0;
                 simplenavElement.each(function () {
-                    var data= {};
+                    var data = {};
+                    data.instance = instance++;
+                    data.settings = settings;
                     data.element = $(this);
                     data.breaks = [];
                     _this.prepareHtml(data);
@@ -32,10 +38,11 @@
 
 
             prepareHtml: function (data) {
+                if (data.element.find(data.settings.toggle).length) return;
                 data.element.append('' +
-                    '<li class="js-mainnav-toggle-wrapper" style="display:none;">' +
-                    '<a href="#" class="u-relative c-nav__list-item c-nav__link ">toggle</a>' +
-                    '<ul style="position: absolute;" class="js-mainnav-dropdown"></ul>' +
+                    '<li class="' + data.settings.toggleWrapper + '" style="display:none;">' +
+                        '<button id="menu-button-'+data.instance+'" aria-label="Menu" aria-expanded="false" aria-controls="menu-'+data.instance+'" type="button" class="' + data.settings.toggle + '">toggle</a>' +
+                        '<ul id="menu-'+data.instance+'" aria-hidden="true" aria-labelledby="menu-button-'+data.instance+'" style="position: absolute;" class="' + data.settings.dropdown + '"></ul>' +
                     '</li>' +
                     '')
             },
@@ -45,9 +52,9 @@
              * Update variables
              */
             checkVars: function (data) {
-                data.toggleWidth = data.element.find('.js-mainnav-toggle-wrapper').outerWidth();
+                data.toggleWidth = data.element.find('.' + data.settings.toggleWrapper).outerWidth();
                 data.viewportWidth = $(window).width();
-                data.menuHeight = data.element.outerHeight();
+                data.menuHeight = data.element.height();
                 data.itemHeight = this.maxHeight(data.element.children('li'));
                 data.lowestViewport = this.getLowestViewportBreak(data.breaks);
             },
@@ -71,33 +78,42 @@
              *
              * @param selector must be jQuery object
              */
-            maxHeight: function(selector){
+            maxHeight: function (selector) {
                 var maxHeight = 0;
-                selector.each(function(){
+                selector.each(function () {
+                    if (!$(this).is(":visible")) return;
                     var height = $(this).outerHeight();
-                    if (height > maxHeight) { maxHeight = height; }
+                    if (height > maxHeight) {
+                        maxHeight = height;
+                    }
                 })
                 return maxHeight;
             },
 
             /**
              * Check if we have to move an item to dropdown
+             * [menu item]--->>[dropdown]
              */
             checkMove: function (data) {
-                while (data.menuHeight > data.itemHeight && data.element.children('li').length > 0) {
+                console.log('menuHeight: ' + data.menuHeight);
+                console.log('itemHeight: ' + data.itemHeight);
+                var i = 0;
+                while (data.menuHeight > data.itemHeight + 5 && data.element.children('li').length > 0 && i < 15) {
+                    i++;
                     var item = data.element.children('li:nth-last-child(2)');
                     this.moveItem(item, data);
                     this.checkVars(data);
                 }
             },
             moveItem: function (element, data) {
-                element.prependTo(data.element.find('.js-mainnav-dropdown'));
+                element.prependTo(data.element.find('.' + data.settings.dropdown));
                 data.breaks.push({'break': data.viewportWidth + data.toggleWidth});
             },
 
 
             /**
              * Check if we have to retrieve an item
+             * [dropdown item]--->>[menu]
              */
             checkRetrieve: function (data) {
                 while (data.viewportWidth > data.lowestViewport && data.breaks.length > 0) {
@@ -106,25 +122,33 @@
                 }
             },
             retrieveItem: function (data) {
-                var item = data.element.find('.js-mainnav-dropdown').children('li:first-child');
+                var item = data.element.find('.' + data.settings.dropdown).children('li:first-child');
                 item.insertBefore(data.element.children('li:last-child'));
                 data.breaks.pop();
             },
 
 
+            /**
+             * Check if we have to show the more dropdown
+             * @param data
+             */
             checkDropdown: function (data) {
                 if (data.breaks.length > 0) {
-                    data.element.find('.js-mainnav-toggle-wrapper').show();
+                    data.element.find('.' + data.settings.toggleWrapper).show();
+                    $('.' + data.settings.toggle).attr('aria-expanded', 'true');
                     this.checkVars(data);
                     this.checkMove(data);
                 } else {
-                    data.element.find('.js-mainnav-toggle-wrapper').hide();
+                    data.element.find('.' + data.settings.toggleWrapper).hide();
                     this.checkVars(data);
                     this.checkRetrieve(data);
                 }
             },
 
-
+            /**
+             * Main logic check
+             * @param data
+             */
             check: function (data) {
                 this.checkVars(data);
                 this.checkMove(data);
@@ -133,16 +157,41 @@
             },
 
 
+            /**
+             * Debounced resize for optimized resize event
+             *
+             * source: https://davidwalsh.name/javascript-debounce-function
+             * @param func
+             * @param wait
+             * @param immediate
+             * @returns {Function}
+             */
+            debounce: function (func, wait, immediate) {
+                var timeout;
+                return function () {
+                    var context = this, args = arguments;
+                    var later = function () {
+                        timeout = null;
+                        if (!immediate) func.apply(context, args);
+                    };
+                    var callNow = immediate && !timeout;
+                    clearTimeout(timeout);
+                    timeout = setTimeout(later, wait);
+                    if (callNow) func.apply(context, args);
+                };
+            },
+
             trigger: function (data) {
-                $(window).on('resize', function () {
-                    app.check(data);
-                })
+                // Bind resize event to window object
+                var check = this.debounce(function () {
+                    app.check(data)
+                }, data.settings.throttle);
+                $(window).on('resize', check);
             }
         }
 
         app.init();
 
-        return 'lol';
     };
 
-}( jQuery ));
+}(jQuery));
